bb7089a78aef0a5033fd5a0d7a68688001ba0ae4
Stefan Böcker <stefan@profboecker.eu>
2025-06-28 11:29:16 +0200

    Description file updated.

--- R/installation.R
+++ R/installation.R
@@ -10,7 +10,7 @@ install_fhswf <- function(){
   pkgs <- c("caption", "xpatch", "csquotes", "verbatimbox", "float","enumitem",
             "elsarticle","pdfcolmk","eurosym","paralist","sectsty","subfig",
             "lineno","algorithm2e","ifoddpage","relsize","setspace",
-            "microtype","fp","ms","pgf","babel-english","biblatex",
+            "microtype","fp","pgf","babel-english","babel-german", "biblatex",
             "logreq","grffile","bera","fancyhdr","titlesec","palatino",
             "mathtools","eukdate","textpos","grfext","fpl","mathpazo","biber",
             "colortbl", "wrapfig", "pdflscape", "xcolor", "multirow", "carlito")
